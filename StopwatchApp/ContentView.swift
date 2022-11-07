2d32f052bd94eebe35480a4512d595bc4895819c
Russell Gordon <rgordon@lcs.on.ca>
2022-11-07 06:29:43 -0500

    Made the background color.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            // First layer (background)
+            Color.yellow
+                .ignoresSafeArea()
+            
+            // Second layer (rest of interface)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
