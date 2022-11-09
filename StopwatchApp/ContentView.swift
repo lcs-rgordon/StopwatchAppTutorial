393454ae9a2f1de21100d3431dfadfeb910f98d7
Russell Gordon <rgordon@lcs.on.ca>
2022-11-09 10:38:44 -0500

    Made list style better match our goal; also adjusted list item insets.

--- StopwatchApp/ContentView.swift
+++ StopwatchApp/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                     CircleButtonView(buttonColor: Color("Dark Grey"),
                                      label: "Reset",
                                      labelColor: .white)
-
+                    
                     Spacer()
                     
                     CircleButtonView(buttonColor: Color("Dark Green"),
@@ -39,15 +39,23 @@ struct ContentView: View {
                 
                 // List of times
                 List {
-                    Text("1")
-                    Text("2")
-                    Text("3")
-                    Text("4")
-                    Text("5")
+                    
+                    Group {
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                        Text("4")
+                        Text("5")
+                    }
+                    // Remove inset from list items
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)) 
+                    
                 }
                 // Set the amount of vertical height we want this list to take up
                 .frame(height: 300)
-
+                // Adjust list style to match design
+                .listStyle(.plain)
+                
             }
             .padding()
         }
